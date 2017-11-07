c99a28564cc63b2f1ee09ef7d3c5d3c6b5320ef6
j_thinker <847713898@qq.com>
2017-11-07 22:55:05 +0800

    .

--- src/com/ucaizhi/bio/TimeServerHandler.java
+++ src/com/ucaizhi/bio/TimeServerHandler.java
@@ -7,7 +7,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Date;
 
-public class TimeServerHandler implements Runnable{
+public class TimeServerHandler implements Runnable {
 
 	private Socket socket = null;
 
@@ -22,32 +22,32 @@ public class TimeServerHandler implements Runnable{
 		PrintWriter out = null;
 		try {
 			in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-			out = new PrintWriter(this.socket.getOutputStream(),true);
+			out = new PrintWriter(this.socket.getOutputStream(), true);
 			String currentTime = null;
 			String body = null;
-			while(true){
-				body= in.readLine();
-				if(body == null){
+			while (true) {
+				body = in.readLine();
+				if (body == null) {
 					break;
 				}
 				System.out.println("The time server receive order:" + body);
-				currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString():"BAD ORDER";
+				currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)
+						? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
 				out.println(currentTime);
 			}
 		} catch (IOException e) {
-			if(in != null){
+			if (in != null) {
 				try {
 					in.close();
 				} catch (IOException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
-			if(out != null){
+			if (out != null) {
 				out.close();
 				out = null;
 			}
-			if(this.socket != null){
+			if (this.socket != null) {
 				try {
 					this.socket.close();
 				} catch (IOException e1) {
@@ -56,9 +56,7 @@ public class TimeServerHandler implements Runnable{
 				this.socket = null;
 			}
 		}
-		
+
 	}
-	
-	
-	
+
 }
